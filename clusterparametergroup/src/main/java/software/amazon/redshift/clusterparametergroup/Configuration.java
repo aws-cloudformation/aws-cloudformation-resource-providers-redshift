9047356a2181545ed79fcab1cdc6b2cfe52326d8
Zirui Hua <huzirui@amazon.com>
2020-08-05 14:52:50 -0700

    Add AWS::Redshift::ClusterParameterGroup

---
+++ clusterparametergroup/src/main/java/software/amazon/redshift/clusterparametergroup/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.redshift.clusterparametergroup;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-redshift-clusterparametergroup.json");
+    }
+}
