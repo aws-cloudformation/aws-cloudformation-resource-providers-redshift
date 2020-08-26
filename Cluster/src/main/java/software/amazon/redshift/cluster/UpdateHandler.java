97ccc949991f1eac12b29cf61246f114e703d06c
Shweta Yakkali <syakkali@amazon.com>
2020-08-26 11:52:54 -0700

    Update  Handler and Unit test

---
+++ Cluster/src/main/java/software/amazon/redshift/cluster/UpdateHandler.java
@@ -0,0 +1,120 @@
+package software.amazon.redshift.cluster;
+
+// TODO: replace all usage of SdkClient with your service client type, e.g; YourServiceAsyncClient
+// import software.amazon.awssdk.services.yourservice.YourServiceAsyncClient;
+
+import software.amazon.awssdk.awscore.AwsResponse;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.services.redshift.RedshiftClient;
+import software.amazon.awssdk.services.redshift.model.Cluster;
+import software.amazon.awssdk.services.redshift.model.ClusterNotFoundException;
+import software.amazon.awssdk.services.redshift.model.ClusterParameterGroupNotFoundException;
+import software.amazon.awssdk.services.redshift.model.ClusterSecurityGroupNotFoundException;
+import software.amazon.awssdk.services.redshift.model.ClusterSubnetGroupNotFoundException;
+import software.amazon.awssdk.services.redshift.model.ClusterSubnetQuotaExceededException;
+import software.amazon.awssdk.services.redshift.model.DependentServiceRequestThrottlingException;
+import software.amazon.awssdk.services.redshift.model.DescribeClustersResponse;
+import software.amazon.awssdk.services.redshift.model.InvalidClusterSecurityGroupStateException;
+import software.amazon.awssdk.services.redshift.model.InvalidClusterStateException;
+import software.amazon.awssdk.services.redshift.model.InvalidClusterTrackException;
+import software.amazon.awssdk.services.redshift.model.InvalidElasticIpException;
+import software.amazon.awssdk.services.redshift.model.InvalidRetentionPeriodException;
+import software.amazon.awssdk.services.redshift.model.InvalidSubnetException;
+import software.amazon.awssdk.services.redshift.model.LimitExceededException;
+import software.amazon.awssdk.services.redshift.model.ModifyClusterRequest;
+import software.amazon.awssdk.services.redshift.model.ModifyClusterResponse;
+import software.amazon.awssdk.services.redshift.model.ModifyClusterSubnetGroupRequest;
+import software.amazon.awssdk.services.redshift.model.ModifyClusterSubnetGroupResponse;
+import software.amazon.awssdk.services.redshift.model.RedshiftException;
+import software.amazon.awssdk.services.redshift.model.SubnetAlreadyInUseException;
+import software.amazon.awssdk.services.redshift.model.UnauthorizedOperationException;
+import software.amazon.awssdk.services.redshift.model.UnsupportedOptionException;
+import software.amazon.cloudformation.exceptions.CfnGeneralServiceException;
+import software.amazon.cloudformation.exceptions.CfnInvalidRequestException;
+import software.amazon.cloudformation.exceptions.CfnNotFoundException;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.ProxyClient;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+
+import java.util.List;
+
+public class UpdateHandler extends BaseHandlerStd {
+    private Logger logger;
+
+    protected ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+            final AmazonWebServicesClientProxy proxy,
+            final ResourceHandlerRequest<ResourceModel> request,
+            final CallbackContext callbackContext,
+            final ProxyClient<RedshiftClient> proxyClient,
+            final Logger logger) {
+
+        this.logger = logger;
+
+        final ResourceModel model = request.getDesiredResourceState();
+
+        return ProgressEvent.progress(model, callbackContext)
+                .then(progress -> proxy.initiate("AWS-Redshift-Cluster::Update", proxyClient, model, callbackContext)
+                        .translateToServiceRequest(Translator::translateToUpdateRequest)
+                        .makeServiceCall(this::updateResource)
+                        .progress())
+                .then(progress -> handleModifyRequest(request, proxyClient, proxy, progress))
+                .then(progress -> new ReadHandler().handleRequest(proxy, request, callbackContext, proxyClient, logger));
+    }
+
+    private ModifyClusterResponse updateResource(
+            final ModifyClusterRequest modifyRequest,
+            final ProxyClient<RedshiftClient> proxyClient) {
+        ModifyClusterResponse awsResponse = null;
+
+        try {
+            awsResponse = proxyClient.injectCredentialsAndInvokeV2(modifyRequest, proxyClient.client()::modifyCluster);
+        } catch (final InvalidClusterStateException | InvalidClusterSecurityGroupStateException | UnauthorizedOperationException
+                | UnsupportedOptionException | LimitExceededException | InvalidElasticIpException | InvalidClusterTrackException | InvalidRetentionPeriodException
+                | DependentServiceRequestThrottlingException | ClusterSubnetQuotaExceededException e ) {
+            throw new CfnInvalidRequestException(modifyRequest.toString(), e);
+        } catch (final ClusterNotFoundException | ClusterSecurityGroupNotFoundException | ClusterParameterGroupNotFoundException e) {
+            throw new CfnNotFoundException(ResourceModel.TYPE_NAME, modifyRequest.clusterIdentifier());
+        } catch (SdkClientException | RedshiftException e) {
+            throw new CfnGeneralServiceException(modifyRequest.toString(), e);
+        }
+
+        logger.log(String.format("%s has successfully been updated.", ResourceModel.TYPE_NAME));
+        return awsResponse;
+    }
+
+    private ProgressEvent<ResourceModel, CallbackContext> handleModifyRequest(
+            ResourceHandlerRequest<ResourceModel> request,
+            final ProxyClient<RedshiftClient> proxyClient,
+            final AmazonWebServicesClientProxy proxy,
+            final ProgressEvent<ResourceModel, CallbackContext> progress) {
+
+        // retrieve cluster identifier from request
+        String clusterIdentifier = request.getDesiredResourceState().getClusterIdentifier();
+
+        // retrieve cluster from cluster identifier
+        DescribeClustersResponse describeClustersResponse = Translator.describeClustersResponse(clusterIdentifier, proxy, proxyClient);
+
+        Cluster cluster = describeClustersResponse.clusters().get(0);
+
+//        if (!cluster.nodeType().equals(request.getDesiredResourceState().getNodeType())) {
+//            proxy.injectCredentialsAndInvokeV2(Translator.modifyClusterNodeType(
+//                    request.getDesiredResourceState().getNodeType(), clusterIdentifier),
+//                    proxyClient.client()::modifyCluster);
+//        } else if (cluster.numberOfNodes()!= request.getDesiredResourceState().getNumberOfNodes()) {
+//            proxy.injectCredentialsAndInvokeV2(Translator.modifyClusterNodeType(
+//                    request.getDesiredResourceState().getNumberOfNodes(), clusterIdentifier),
+//                    proxyClient.client()::modifyCluster);
+//        }
+
+        //check what is different for the cluster via modify request
+        // use several nested if-else to see what is different in the new modify request for the cluster.
+        // e.g if node type change then API call to create new cluster with this new node type
+        // and then delete the previous one?
+        //modify
+
+        return progress;
+    }
+}
---
+++ Cluster/src/test/java/software/amazon/redshift/cluster/UpdateHandlerTest.java
@@ -0,0 +1,168 @@
+package software.amazon.redshift.cluster;
+
+import java.time.Duration;
+import software.amazon.awssdk.core.SdkClient;
+import software.amazon.awssdk.services.redshift.RedshiftClient;
+import software.amazon.awssdk.services.redshift.model.Cluster;
+import software.amazon.awssdk.services.redshift.model.DescribeClustersRequest;
+import software.amazon.awssdk.services.redshift.model.DescribeClustersResponse;
+import software.amazon.awssdk.services.redshift.model.ModifyClusterRequest;
+import software.amazon.awssdk.services.redshift.model.ModifyClusterResponse;
+import software.amazon.awssdk.services.redshift.model.ModifyClusterSubnetGroupRequest;
+import software.amazon.awssdk.services.redshift.model.ModifyClusterSubnetGroupResponse;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.ProxyClient;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static software.amazon.redshift.cluster.TestUtils.BASIC_CLUSTER;
+import static software.amazon.redshift.cluster.TestUtils.BASIC_MODEL;
+import static software.amazon.redshift.cluster.TestUtils.CLUSTER_IDENTIFIER;
+import static software.amazon.redshift.cluster.TestUtils.MASTER_USERNAME;
+import static software.amazon.redshift.cluster.TestUtils.NODETYPE;
+import static software.amazon.redshift.cluster.TestUtils.NUMBER_OF_NODES;
+import static software.amazon.redshift.cluster.TestUtils.ALLOWVERSIONUPGRADE;
+import static software.amazon.redshift.cluster.TestUtils.AUTOMATEDSNAPSHOTRETENTIONPERIOD;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateHandlerTest extends AbstractTestBase {
+
+    @Mock
+    private AmazonWebServicesClientProxy proxy;
+
+    @Mock
+    private ProxyClient<RedshiftClient> proxyClient;
+
+    @Mock
+    RedshiftClient sdkClient;
+
+    static UpdateHandler handler;
+    @BeforeEach
+    public void setup() {
+        handler = new UpdateHandler();
+        proxy = new AmazonWebServicesClientProxy(logger, MOCK_CREDENTIALS, () -> Duration.ofSeconds(600).toMillis());
+        sdkClient = mock(RedshiftClient.class);
+        proxyClient = MOCK_PROXY(proxy, sdkClient);
+    }
+
+    @AfterEach
+    public void tear_down() {
+        verify(sdkClient, atLeastOnce()).serviceName();
+        verifyNoMoreInteractions(sdkClient);
+    }
+
+    @Test
+    public void handleRequest_SimpleSuccess() {
+        final ResourceModel model = ResourceModel.builder()
+                .clusterIdentifier(CLUSTER_IDENTIFIER)
+                .masterUsername(MASTER_USERNAME)
+                .nodeType("dc2.large")
+                .numberOfNodes(NUMBER_OF_NODES)
+                .allowVersionUpgrade(ALLOWVERSIONUPGRADE)
+                .automatedSnapshotRetentionPeriod(AUTOMATEDSNAPSHOTRETENTIONPERIOD)
+                .build();
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(model)
+            .build();
+
+
+        Cluster modifiedCluster = Cluster.builder()
+                .clusterIdentifier(CLUSTER_IDENTIFIER)
+                .masterUsername(MASTER_USERNAME)
+                .nodeType("dc2.large")
+                .numberOfNodes(NUMBER_OF_NODES)
+                .allowVersionUpgrade(ALLOWVERSIONUPGRADE)
+                .automatedSnapshotRetentionPeriod(AUTOMATEDSNAPSHOTRETENTIONPERIOD)
+                .build();
+
+        when(proxyClient.client().modifyCluster(any(ModifyClusterRequest.class)))
+                .thenReturn(ModifyClusterResponse.builder()
+                        .cluster(modifiedCluster)
+                        .build());
+
+        when(proxyClient.client().describeClusters(any(DescribeClustersRequest.class)))
+                .thenReturn(DescribeClustersResponse.builder()
+                        .clusters(BASIC_CLUSTER) //ds2.xlarge node type
+                        .build())
+                .thenReturn(DescribeClustersResponse.builder()
+                        .clusters(modifiedCluster) //dc2.large node type
+                        .build());
+
+
+        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, new CallbackContext(), proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModel()).isEqualTo(request.getDesiredResourceState());
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isNull();
+    }
+
+    @Test
+    public void testModifyNumberOfNodes() {
+        final ResourceModel model = ResourceModel.builder()
+                .clusterIdentifier(CLUSTER_IDENTIFIER)
+                .masterUsername(MASTER_USERNAME)
+                .nodeType(NODETYPE)
+                .numberOfNodes(NUMBER_OF_NODES * 2)
+                .allowVersionUpgrade(ALLOWVERSIONUPGRADE)
+                .automatedSnapshotRetentionPeriod(AUTOMATEDSNAPSHOTRETENTIONPERIOD)
+                .build();
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(model)
+                .build();
+
+
+        Cluster modifiedCluster = Cluster.builder()
+                .clusterIdentifier(CLUSTER_IDENTIFIER)
+                .masterUsername(MASTER_USERNAME)
+                .nodeType(NODETYPE)
+                .numberOfNodes(NUMBER_OF_NODES * 2)
+                .allowVersionUpgrade(ALLOWVERSIONUPGRADE)
+                .automatedSnapshotRetentionPeriod(AUTOMATEDSNAPSHOTRETENTIONPERIOD)
+                .build();
+
+        when(proxyClient.client().modifyCluster(any(ModifyClusterRequest.class)))
+                .thenReturn(ModifyClusterResponse.builder()
+                        .cluster(modifiedCluster)
+                        .build());
+
+        when(proxyClient.client().describeClusters(any(DescribeClustersRequest.class)))
+                .thenReturn(DescribeClustersResponse.builder()
+                        .clusters(BASIC_CLUSTER)
+                        .build())
+                .thenReturn(DescribeClustersResponse.builder()
+                        .clusters(modifiedCluster)
+                        .build());
+
+
+        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, new CallbackContext(), proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModel()).isEqualTo(request.getDesiredResourceState());
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isNull();
+    }
+
+}
