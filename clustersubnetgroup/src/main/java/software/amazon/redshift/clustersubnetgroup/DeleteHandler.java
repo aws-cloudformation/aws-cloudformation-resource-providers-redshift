1985d60026231df4d85830abdc154208bb11cb6b
Xiaoyu Li <xaoyuli@amazon.com>
2020-11-04 16:39:27 -0800

    Set SubnetGroupName as ReadOnly, fix several contract test

--- clustersubnetgroup/src/main/java/software/amazon/redshift/clustersubnetgroup/DeleteHandler.java
+++ clustersubnetgroup/src/main/java/software/amazon/redshift/clustersubnetgroup/DeleteHandler.java
@@ -9,6 +9,7 @@ import software.amazon.awssdk.services.redshift.model.InvalidClusterSubnetStateE
 import software.amazon.cloudformation.exceptions.CfnInvalidRequestException;
 import software.amazon.cloudformation.exceptions.CfnNotFoundException;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.HandlerErrorCode;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ProxyClient;
@@ -32,23 +33,23 @@ public class DeleteHandler extends BaseHandlerStd {
                     proxy.initiate("AWS-Redshift-ClusterSubnetGroup::Delete", proxyClient, model, callbackContext)
                     .translateToServiceRequest(Translator::translateToDeleteRequest)
                     .makeServiceCall(this::deleteResource)
-                    .success());
+                    .handleError((deleteDbSubnetGroupRequest, exception, client, resourceModel, cxt) -> {
+                        if (exception instanceof ClusterSubnetGroupNotFoundException) {
+                            return ProgressEvent.defaultFailureHandler(exception, HandlerErrorCode.NotFound);
+                        }
+                        throw exception;
+                    })
+                    .done((deleteClusterSubnetGroupRequest, deleteClusterSubnetGroupResponse, client, resourceModel, cxt)
+                            -> ProgressEvent.defaultSuccessHandler(null)));
     }
 
     private DeleteClusterSubnetGroupResponse deleteResource(
         final DeleteClusterSubnetGroupRequest deleteRequest,
         final ProxyClient<RedshiftClient> proxyClient) {
-        DeleteClusterSubnetGroupResponse awsResponse = null;
-        try {
-            awsResponse = proxyClient.injectCredentialsAndInvokeV2(deleteRequest, proxyClient.client()::deleteClusterSubnetGroup);
-            logger.log(String.format("%s [%s] Deleted Successfully", ResourceModel.TYPE_NAME, deleteRequest.clusterSubnetGroupName()));
-        } catch (final ClusterSubnetGroupNotFoundException e) {
-            throw new CfnNotFoundException(ResourceModel.TYPE_NAME, deleteRequest.clusterSubnetGroupName());
-        } catch (final InvalidClusterSubnetGroupStateException | InvalidClusterSubnetStateException e) {
-            throw new CfnInvalidRequestException(deleteRequest.toString(), e);
-        }
+        DeleteClusterSubnetGroupResponse awsResponse = proxyClient.injectCredentialsAndInvokeV2(deleteRequest,
+                proxyClient.client()::deleteClusterSubnetGroup);
+        logger.log(String.format("%s [%s] Deleted Successfully", ResourceModel.TYPE_NAME, deleteRequest.clusterSubnetGroupName()));
 
-        logger.log(String.format("%s successfully deleted.", ResourceModel.TYPE_NAME));
         return awsResponse;
     }
 }
