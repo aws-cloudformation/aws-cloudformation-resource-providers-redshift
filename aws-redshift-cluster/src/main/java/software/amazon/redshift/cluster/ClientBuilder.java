595134ddce6a79a8962e458dec581a1edc5b48b7
gawoso8802 <122635363+gawoso8802@users.noreply.github.com>
2023-01-13 23:27:43 +0530

    Update ClientBuilder.java

--- aws-redshift-cluster/src/main/java/software/amazon/redshift/cluster/ClientBuilder.java
+++ aws-redshift-cluster/src/main/java/software/amazon/redshift/cluster/ClientBuilder.java
@@ -1,8 +1,6 @@
 package software.amazon.redshift.cluster;
-
 import software.amazon.awssdk.services.redshift.RedshiftClient;
 import software.amazon.cloudformation.LambdaWrapper;
-
 public class ClientBuilder {
   static RedshiftClient getClient() {
     return RedshiftClient.builder()
