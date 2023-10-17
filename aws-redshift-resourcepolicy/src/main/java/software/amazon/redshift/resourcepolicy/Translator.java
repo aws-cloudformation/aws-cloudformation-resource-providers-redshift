663ca7e1098aedb3a246468b3e14f22795388002
Shantagouda Kuntoji <shantagk@amazon.com>
2023-10-17 14:10:08 -0700

    Cfn support for ResourcePolicy

---
+++ aws-redshift-resourcepolicy/src/main/java/software/amazon/redshift/resourcepolicy/Translator.java
@@ -0,0 +1,155 @@
+package software.amazon.redshift.resourcepolicy;
+
+import com.google.common.collect.Lists;
+import software.amazon.awssdk.awscore.AwsRequest;
+import software.amazon.awssdk.awscore.AwsResponse;
+
+import software.amazon.awssdk.services.redshift.model.PutResourcePolicyRequest;
+import software.amazon.awssdk.services.redshift.model.PutResourcePolicyResponse;
+import software.amazon.awssdk.services.redshift.model.GetResourcePolicyRequest;
+import software.amazon.awssdk.services.redshift.model.GetResourcePolicyResponse;
+import software.amazon.awssdk.services.redshift.model.DeleteResourcePolicyRequest;
+import software.amazon.awssdk.services.redshift.model.DeleteResourcePolicyResponse;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * This class is a centralized placeholder for
+ *  - api request construction
+ *  - object translation to/from aws sdk
+ *  - resource model construction for read/list handlers
+ */
+
+public class Translator {
+
+  /**
+   * Request to create a resource
+   * @param model resource model
+   * @return awsRequest the aws service request to create a resource
+   */
+  static PutResourcePolicyRequest translateToCreateRequest(final ResourceModel model) {
+    return PutResourcePolicyRequest.builder()
+            .resourceArn(model.getResourceArn())
+            .policy(model.getPolicy())
+            .build();
+  }
+
+  /**
+   * Request to read a resource
+   * @param model resource model
+   * @return awsRequest the aws service request to describe a resource
+   */
+  static GetResourcePolicyRequest translateToReadRequest(final ResourceModel model) {
+    return GetResourcePolicyRequest.builder()
+            .resourceArn(model.getResourceArn())
+            .build();
+  }
+
+  /**
+   * Translates resource object from sdk into a resource model
+   * @param awsResponse the aws service describe resource response
+   * @return model resource model
+   */
+  static ResourceModel translateFromReadResponse(final GetResourcePolicyResponse getResponse) {
+    return ResourceModel.builder()
+            .resourceArn(getResponse.resourcePolicy().resourceArn())
+            .policy(getResponse.resourcePolicy().policy())
+            .build();
+  }
+
+  /**
+   * Request to delete a resource
+   * @param model resource model
+   * @return awsRequest the aws service request to delete a resource
+   */
+  static DeleteResourcePolicyRequest translateToDeleteRequest(final ResourceModel model) {
+    return DeleteResourcePolicyRequest.builder()
+            .resourceArn(model.getResourceArn())
+            .build();
+  }
+
+  /**
+   * Request to update properties of a previously created resource
+   * @param model resource model
+   * @return awsRequest the aws service request to modify a resource
+   */
+  static AwsRequest translateToFirstUpdateRequest(final ResourceModel model) {
+    final AwsRequest awsRequest = null;
+    // TODO: construct a request
+    // e.g. https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-logs/blob/2077c92299aeb9a68ae8f4418b5e932b12a8b186/aws-logs-loggroup/src/main/java/com/aws/logs/loggroup/Translator.java#L45-L50
+    return awsRequest;
+  }
+
+  /**
+   * Request to update some other properties that could not be provisioned through first update request
+   * @param model resource model
+   * @return awsRequest the aws service request to modify a resource
+   */
+  static AwsRequest translateToSecondUpdateRequest(final ResourceModel model) {
+    final AwsRequest awsRequest = null;
+    // TODO: construct a request
+    return awsRequest;
+  }
+
+  /**
+   * Request to list resources
+   * @param nextToken token passed to the aws service list resources request
+   * @return awsRequest the aws service request to list resources within aws account
+   */
+  static AwsRequest translateToListRequest(final String nextToken) {
+    final AwsRequest awsRequest = null;
+    // TODO: construct a request
+    // e.g. https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-logs/blob/2077c92299aeb9a68ae8f4418b5e932b12a8b186/aws-logs-loggroup/src/main/java/com/aws/logs/loggroup/Translator.java#L26-L31
+    return awsRequest;
+  }
+
+  /**
+   * Translates resource objects from sdk into a resource model (primary identifier only)
+   * @param awsResponse the aws service describe resource response
+   * @return list of resource models
+   */
+  static List<ResourceModel> translateFromListRequest(final AwsResponse awsResponse) {
+    // e.g. https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-logs/blob/2077c92299aeb9a68ae8f4418b5e932b12a8b186/aws-logs-loggroup/src/main/java/com/aws/logs/loggroup/Translator.java#L75-L82
+    return streamOfOrEmpty(Lists.newArrayList())
+            .map(resource -> ResourceModel.builder()
+                    // include only primary identifier
+                    .build())
+            .collect(Collectors.toList());
+  }
+
+  private static <T> Stream<T> streamOfOrEmpty(final Collection<T> collection) {
+    return Optional.ofNullable(collection)
+            .map(Collection::stream)
+            .orElseGet(Stream::empty);
+  }
+
+  /**
+   * Request to add tags to a resource
+   * @param model resource model
+   * @return awsRequest the aws service request to create a resource
+   */
+  static AwsRequest tagResourceRequest(final ResourceModel model, final Map<String, String> addedTags) {
+    final AwsRequest awsRequest = null;
+    // TODO: construct a request
+    // e.g. https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-logs/blob/2077c92299aeb9a68ae8f4418b5e932b12a8b186/aws-logs-loggroup/src/main/java/com/aws/logs/loggroup/Translator.java#L39-L43
+    return awsRequest;
+  }
+
+  /**
+   * Request to add tags to a resource
+   * @param model resource model
+   * @return awsRequest the aws service request to create a resource
+   */
+  static AwsRequest untagResourceRequest(final ResourceModel model, final Set<String> removedTags) {
+    final AwsRequest awsRequest = null;
+    // TODO: construct a request
+    // e.g. https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-logs/blob/2077c92299aeb9a68ae8f4418b5e932b12a8b186/aws-logs-loggroup/src/main/java/com/aws/logs/loggroup/Translator.java#L39-L43
+    return awsRequest;
+  }
+}
---
+++ aws-redshift-resourcepolicy/src/test/java/software/amazon/redshift/resourcepolicy/AbstractTestBase.java
@@ -0,0 +1,122 @@
+package software.amazon.redshift.resourcepolicy;
+
+import java.lang.UnsupportedOperationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import software.amazon.awssdk.awscore.AwsRequest;
+import software.amazon.awssdk.awscore.AwsResponse;
+import software.amazon.awssdk.services.redshift.RedshiftClient;
+import software.amazon.awssdk.services.redshift.model.*;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.pagination.sync.SdkIterable;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Credentials;
+import software.amazon.cloudformation.proxy.LoggerProxy;
+import software.amazon.cloudformation.proxy.ProxyClient;
+
+import static software.amazon.awssdk.services.redshift.model.PutResourcePolicyResponse.*;
+
+public class AbstractTestBase {
+  protected static final Credentials MOCK_CREDENTIALS;
+  protected static final LoggerProxy logger;
+
+  protected static final String RESOURCE_ARN;
+
+  protected static final String POLICY;
+
+  protected static final ResourcePolicy RESOURCE_POLICY;
+
+  static {
+    MOCK_CREDENTIALS = new Credentials("accessKey", "secretKey", "token");
+    logger = new LoggerProxy();
+    RESOURCE_ARN = "DummyResourceArn";
+    POLICY = "DummyResourcePolicy";
+
+    RESOURCE_POLICY = ResourcePolicy.builder()
+            .resourceArn(RESOURCE_ARN)
+            .policy(POLICY)
+            .build();
+  }
+  static ProxyClient<RedshiftClient> MOCK_PROXY(
+          final AmazonWebServicesClientProxy proxy,
+          final RedshiftClient sdkClient) {
+    return new ProxyClient<RedshiftClient>() {
+      @Override
+      public <RequestT extends AwsRequest, ResponseT extends AwsResponse> ResponseT
+      injectCredentialsAndInvokeV2(RequestT request, Function<RequestT, ResponseT> requestFunction) {
+        return proxy.injectCredentialsAndInvokeV2(request, requestFunction);
+      }
+
+      @Override
+      public <RequestT extends AwsRequest, ResponseT extends AwsResponse>
+      CompletableFuture<ResponseT>
+      injectCredentialsAndInvokeV2Async(RequestT request, Function<RequestT, CompletableFuture<ResponseT>> requestFunction) {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public <RequestT extends AwsRequest, ResponseT extends AwsResponse, IterableT extends SdkIterable<ResponseT>>
+      IterableT
+      injectCredentialsAndInvokeIterableV2(RequestT request, Function<RequestT, IterableT> requestFunction) {
+        return proxy.injectCredentialsAndInvokeIterableV2(request, requestFunction);
+      }
+
+      @Override
+      public <RequestT extends AwsRequest, ResponseT extends AwsResponse> ResponseInputStream<ResponseT>
+      injectCredentialsAndInvokeV2InputStream(RequestT requestT, Function<RequestT, ResponseInputStream<ResponseT>> function) {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public <RequestT extends AwsRequest, ResponseT extends AwsResponse> ResponseBytes<ResponseT>
+      injectCredentialsAndInvokeV2Bytes(RequestT requestT, Function<RequestT, ResponseBytes<ResponseT>> function) {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public RedshiftClient client() {
+        return sdkClient;
+      }
+    };
+  }
+
+  public static ResourceModel putResourcePolicyRequestResourceModel() {
+    return ResourceModel.builder()
+            .resourceArn(RESOURCE_ARN)
+            .policy(POLICY)
+            .build();
+  }
+
+  public static PutResourcePolicyResponse putResourcePolicyResponseSdk() {
+    return PutResourcePolicyResponse.builder()
+            .resourcePolicy(RESOURCE_POLICY)
+            .build();
+  }
+
+  public static ResourceModel getResourcePolicyRequestResourceModel() {
+    return ResourceModel.builder()
+            .resourceArn(RESOURCE_ARN)
+            .build();
+  }
+
+  public static ResourceModel getResourcePolicyResponseResourceModel() {
+    return putResourcePolicyRequestResourceModel();
+  }
+
+  public static GetResourcePolicyResponse getResourcePolicyResponseSdk() {
+    return GetResourcePolicyResponse.builder()
+            .resourcePolicy(RESOURCE_POLICY)
+            .build();
+  }
+
+  public static ResourceModel deleteResourcePolicyRequestResourceModel() {
+    return ResourceModel.builder()
+            .resourceArn(RESOURCE_ARN)
+            .build();
+  }
+
+  public static DeleteResourcePolicyResponse deleteResourcePolicyResponseSdk() {
+    return DeleteResourcePolicyResponse.builder().build();
+  }
+}
