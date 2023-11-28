3cb061b92f36d2d0149e348f822fbce2fb95db96
Harika Kondru <hkondru@amazon.com>
2023-11-28 10:35:57 -0800

    Add Stack tags support to create-cluster call
    The create-cluster can be tagged with only resource-level tags, stack level tags are not added as the part of this process to the resource.
    Stack Level Tags: are tags specified in the AWS console, they should be shared by all resources created inside that stack.
    Resource Level Tags: are tags specified in CloudFormation templates.
    CloudFormation System Tags: In addition to any tags you define, CloudFormation automatically creates the following system tags with the prefix aws:
    * aws:cloudformation:logical-id
    * aws:cloudformation:stack-id
    * aws:cloudformation:stack-name

--- aws-redshift-cluster/src/main/java/software/amazon/redshift/cluster/Translator.java
+++ aws-redshift-cluster/src/main/java/software/amazon/redshift/cluster/Translator.java
@@ -50,6 +50,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -71,7 +72,7 @@ public class Translator {
    * @param model resource model
    * @return awsRequest the aws service request to create a resource
    */
-  static CreateClusterRequest translateToCreateRequest(final ResourceModel model) {
+  static CreateClusterRequest translateToCreateRequest(final ResourceModel model, final Map<String, String> tags) {
     return CreateClusterRequest.builder()
             .clusterIdentifier(model.getClusterIdentifier())
             .masterUsername(model.getMasterUsername())
@@ -97,7 +98,7 @@ public class Translator {
             .publiclyAccessible(model.getPubliclyAccessible())
             .vpcSecurityGroupIds(model.getVpcSecurityGroupIds())
             .iamRoles(model.getIamRoles())
-            .tags(translateTagsToSdk(model.getTags()))
+            .tags(translateTagsToSdk(translateTagsMapToTagCollection(tags)))
             .availabilityZoneRelocation(model.getAvailabilityZoneRelocation())
             .aquaConfigurationStatus(model.getAquaConfigurationStatus())
             .manualSnapshotRetentionPeriod(model.getManualSnapshotRetentionPeriod())
@@ -288,6 +289,21 @@ public class Translator {
     return Optional.ofNullable(tags).orElse(Collections.emptyList())
             .stream()
             .map(software.amazon.redshift.cluster.Tag ::getValue)
+            .collect(Collectors.toList());
+  }
+
+  static Map<String, String> translateFromResourceModelToSdkTags(final List<software.amazon.redshift.cluster.Tag> listOfTags){
+
+    Map<String, String> sdkTags = streamOfOrEmpty(listOfTags)
+            .collect(Collectors.toMap(software.amazon.redshift.cluster.Tag::getKey, software.amazon.redshift.cluster.Tag::getValue ));
+
+    return sdkTags.isEmpty() ? null : sdkTags;
+  }
+
+  static List<software.amazon.redshift.cluster.Tag> translateTagsMapToTagCollection(final Map<String, String> tags) {
+    if (tags == null) return null;
+    return tags.keySet().stream()
+            .map(key -> Tag.builder().key(key).value(tags.get(key)).build())
             .collect(Collectors.toList());
   }
 
